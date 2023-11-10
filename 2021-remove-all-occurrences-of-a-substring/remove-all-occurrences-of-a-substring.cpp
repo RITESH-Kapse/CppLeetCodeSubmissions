758e865178e3486bc8c19cab562e3c31652407a9
Ritesh Kapse <68165397+RITESH-Kapse@users.noreply.github.com>
2023-11-10 10:30:07 +0530

    Time: 0 ms (100.00%) | Memory: 6.8 MB (93.74%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -4,6 +4,9 @@ public:
 
         while(s.find(part) != string::npos){
 
+            //string::npos --> matlab pure string me ye "part" found ho gaya
+            // nai find hota to 'npos' matlab no position return karta.
+
             //IF string is find then only we are inside the loop
             //so erase the part from string
 

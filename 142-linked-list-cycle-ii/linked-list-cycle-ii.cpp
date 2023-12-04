f00dcca0749236c5b4ed562a567ebf9a724b52f2
Ritesh Kapse <68165397+RITESH-Kapse@users.noreply.github.com>
2023-12-05 00:48:59 +0530

    Time: 9 ms (41.03%) | Memory: 7.9 MB (71.51%) - LeetSync

---
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+
+        ListNode *slow=head;
+        ListNode *fast=head;
+
+        while(fast != NULL){
+            fast = fast ->next;
+            if(fast != NULL){
+                fast = fast->next;
+                slow=slow->next;
+            }
+
+            if(fast == slow){
+                break;
+            }
+        }
+
+        if(fast==NULL){
+            //no cycle present
+            return NULL;
+        }
+
+        //yaha aaya matlab slow and fast meet kar chuke hai
+        slow=head;
+
+        //slow and fast ek step move karnge
+        while(fast != slow){
+            slow = slow->next;
+            fast = fast->next;
+        }
+
+        //return starting point
+        return slow;
+        
+    }
+};
